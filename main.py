d5a06ae71d0343ec2319876ac4515bbcc0614ac1
Manuel <20300545@uttt.edu.mx>
2021-06-30 13:17:57 -0500

    Modificacion del index

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def index():
 
     return render_template('index.html')
 
+@app.route("/gta")
+def gta_html():
+    return render_template('gta.html')
+
+@app.route("/gta5")
+def gta5_html():
+    return render_template('gta5.html')
+
+@app.route("/RedDead")
+def Red_html():
+    return render_template('Red.html')
+
+
 #configuracion de archivo pricipal en ejecucion
 
 if __name__ == '__main__':
